b8a97fbea42be8c0fae992803ce4d66478b0c051
cadowtin <chasdowtin@gmail.com>
2025-06-05 09:21:03 -0500

    2.0.0 - FVTT  V13 compatability

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,22 @@
+## 2.0.0
+- Fvtt `v13` compatability
+
+## 1.0.6
+
+- Fixed issue where messages checked in check for if chat message exists before running delete command were the original stale messages and not the current message list
+
 ## 1.0.5
+
 - Added check for if chat message exists before running delete command
+
 ## 1.0.4
+
 - Removed Languages because it seems to be causing an installation issue
+
 ## 1.0.3
+
 - Fix manifest issue
+
 ## 1.0.2
 
 - Added Languages:
--- scripts/module.js
+++ scripts/module.js
@@ -1,25 +1,35 @@
-Hooks.once('init', async function() {
-    game.settings.register("depruner-chat-message-remover", "limit", {
-        name: game.i18n.localize("depruner-chat-message-remover.module-settings.limit.name"),
-        hint: game.i18n.localize("depruner-chat-message-remover.module-settings.limit.hint"),
-        scope: "world",
-        config: true,
-        default: 30,
-        type: Number,
-    });
+Hooks.once("init", async function () {
+  game.settings.register("depruner-chat-message-remover", "limit", {
+    name: game.i18n.localize(
+      "depruner-chat-message-remover.module-settings.limit.name"
+    ),
+    hint: game.i18n.localize(
+      "depruner-chat-message-remover.module-settings.limit.hint"
+    ),
+    scope: "world",
+    config: true,
+    default: 30,
+    type: Number,
+  });
 });
 
-Hooks.once('ready', async function() {
-    if (!game.user.isGM) return;
-    Hooks.on('preCreateChatMessage', async (_document, _data, _options, _userId) => {
-        const maxMessages = game.settings.get("depruner-chat-message-remover", "limit");
-        const messagesToDelete = game.messages.contents.length - maxMessages;
-        if (messagesToDelete <= 0) return;
-        for (let i = 0; i <= messagesToDelete; i++) {
-            const oldestMessage = game.messages.contents[i];
-            if (oldestMessage) {
-                await oldestMessage?.delete();
-            }
+Hooks.once("ready", async function () {
+  if (!game.user.isGM) return;
+  Hooks.on(
+    "preCreateChatMessage",
+    async (_document, _data, _options, _userId) => {
+      const maxMessages = game.settings.get(
+        "depruner-chat-message-remover",
+        "limit"
+      );
+      const messagesToDelete = game.messages.contents.length - maxMessages;
+      if (messagesToDelete <= 0) return;
+      for (let i = 0; i <= messagesToDelete; i++) {
+        const oldestMessage = game.messages.contents[i];
+        if (oldestMessage) {
+          await oldestMessage?.delete();
         }
-    });
+      }
+    }
+  );
 });
